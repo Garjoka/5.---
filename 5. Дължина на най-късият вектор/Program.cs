51e3aff30bf55a9e90c5f2f0f5ea1e1e2d930258
danibossa <danibossa@DESKTOP-6ROLABH>
2024-06-21 17:47:32 +0200

    Add project files.

---
+++ 5. Дължина на най-късият вектор/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of vectors: ");
+        int N = int.Parse(Console.ReadLine());
+        List<double[]> vectors = new List<double[]>();
+
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write($"Enter vector {i + 1} separated by space: ");
+            double[] vector = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            vectors.Add(vector);
+        }
+
+        double minLength = vectors.Min(vector => Math.Sqrt(vector.Sum(x => x * x)));
+        Console.WriteLine($"Shortest vector length: {minLength}");
+    }
+}
